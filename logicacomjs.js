18316290e80cfe0c1a4fd6c5844b4f07c3f1f00f
lemos10 <Njr99142@gmail.com>
2025-06-03 11:34:00 -0300

    Ninhodeif

--- logicacomjs.js
+++ logicacomjs.js
@@ -26,4 +26,70 @@ console.log("------------------------")*/
 let salario =  1.518;
 let mesestrab = 11;
 let resultado = salario/12*mesestrab
-console.log ("Parabéns "+ nome+" seu decimo é: R$"+ resultado)*/
+console.log ("Parabéns "+ nome+" seu decimo é: R$"+ resultado)
+let salario =  prompt("Digite o seu salario")
+let mesestrab = prompt("Coloque seus meses trabalhados")
+let resultado = salario/12*mesestrab
+console.log ("Parabéns seu 13° é " +  resultado.toFixed(0))
+var salario = prompt("Digite seu salario"){
+var salario novo = porcentagem/porcento*salario+salario
+}
+if (se o salario<R$500,00"){
+    var salarionovo
+    console.log("Seu reajuste vai sar de 15%")
+}
+
+else if (De R$500,00 até 1000,00){
+    console.log("Seu salario recebeu um reajuste de 10%")
+}
+else{
+    console.log("Seu salario recebeu um reajuste de 5%")
+}
+
+let idade = prompt ("Digite aqui sua idade:")
+if (idade<12) {
+    alert ("Assista conteúdo infantil")
+}
+else if (idade>=12 && idade<=17){
+    alert ("Assista conteúdo teen")
+}
+else if (idade>=18 && iadede<=60){
+    alert ("Assista conteúdo Adulto")
+
+}
+else {
+    alert("Assista conteudo Sênior")
+}
+
+let pontuacao = prompt ("Digite sua pontuação:")
+if (pontuacao<=1000){
+    alert ("Seu nível é de Novato e seu bônus de xp é de 100")
+
+}
+else if (pontuacao>=1001 && pontuacao<=5000){
+    alert ("Seu nível é de um Aprendiz e seu bônus de xp é de 500")
+}
+else if (pontuacao>=5001 && até <=10000){
+    alert ("Seu nível é de um Expert e seu bônus de xp é de 1000")
+}
+else { alert ("Acima de 10000 seu nível é de Mestre e seu bônus de xp é de 2000")
+}*/
+
+let valorbasico = 25;
+let valorpremium = 40;
+let valorultra = 60;
+telasde1a2 = 0;
+let telas3a4 = 5;
+let telasmaisde4 = 7;
+let tipodeassinatura = prompt ("Digite o tipo de assinatura: ")
+ let telas = prompt ("Quantas telas deseja?")
+ 
+ if (tipodeassinatura == "basico"){
+    if ( telas>=1 && telas<=2){
+        alert ("O valor do seu pacote sera de R$:" + valorbasico+telasde1a2)
+    }
+    if (tipodeassinatura == "premium")
+     if (telas >=3 && telas <=4){
+alert ("O valor do seu pacote sera de" + premium+telas3a4oumais)
+    }
+}
