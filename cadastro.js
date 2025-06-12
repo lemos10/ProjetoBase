3f3792ad385a4b961dfd5ada218cad2402ad5b28
lemos10 <Njr99142@gmail.com>
2025-06-12 11:31:38 -0300

    Document e event

---
+++ cadastro.js
@@ -0,0 +1,12 @@
+function clicou(){
+    let quandoclicar = document.getElementById("botao")
+    quandoclicar.innertext = "Clicou"
+}
+function saiu(){
+    let quandoclicar = document.getElementById("botao")
+    quandoclicar.style.background = "Green"
+}
+function entrar(){
+    let quandoentrar = document.getElementById("botao")
+    quandoentrar.style.background = "red"
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+/*let buscando = document.getElementsByTagName("11")[2]
+console.log(buscando)
+let buscando = document.getElementsByTagName("h2dadiv")[0]
+console.log(buscando)
+let buscando = document.getElementById("atalaia")
+buscando.style.background = "gray"
+ou:getElementsClassName()
+let modorapido = document.querySelector("div#h3doroteiro")
+modorapido.computedStyleMap.background = "gray"*/
+
